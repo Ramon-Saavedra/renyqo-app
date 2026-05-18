3a5241896444d00a943f6830bde24372b7e91f43
Saavedra Ramon <102835876+Papyrus-Saa@users.noreply.github.com>
2026-05-18 15:24:25 +0200

    fix: enforce no-empty-object-type rule for TypeScript interfaces

--- src/common/types/express.d.ts
+++ src/common/types/express.d.ts
@@ -2,7 +2,6 @@ import type { SafeUser } from '../users/types/safe-user.type';
 
 declare global {
   namespace Express {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface User extends SafeUser {}
   }
 }
